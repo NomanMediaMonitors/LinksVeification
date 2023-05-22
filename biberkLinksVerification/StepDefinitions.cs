4618358c1e28dab045413f60f97044571d40cdc7
Noman Ahmed Khan <128586752+NomanMediaMonitors@users.noreply.github.com>
2023-05-22 19:11:53 +0500

    update

--- biberkLinksVerification/StepDefinitions.cs
+++ biberkLinksVerification/StepDefinitions.cs
@@ -1,103 +1,100 @@
-﻿using NUnit.Framework;
-using System;
+﻿using System;
 using System.Net.Http;
-using System.Threading;
-using TechTalk.SpecFlow;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace biberkLinksVerification
 {
-    [Binding]
-    public class StepDefinitions : Steps
+    public class LinkVerifier
     {
-        private string selectedState;
-        private string pageTitle;
-        private string errorMessage;
-        private string serverMessage;
         private HttpClient httpClient;
 
-        [BeforeScenario]
+        public LinkVerifier()
+        {
+            httpClient = new HttpClient();
+        }
+
+        public async Task VerifyLink(string state)
+        {
+            Setup();
+
+            HttpResponseMessage response_ = await httpClient.GetAsync($"https://www.biberk.com/policyholders/resources/employer-posting-notices/");
+
+            // Check the response status code
+            if (!response_.IsSuccessStatusCode)
+            {
+                string errorMessage = "Expected to see an error message that the page cannot be found";
+                Console.WriteLine(errorMessage);
+            }
+            else
+            {
+                string message = "Landed On the Mainpage";
+                Console.WriteLine(message);
+            }
+
+            // Send an HTTP GET request to the page URL
+            HttpResponseMessage response__ = await httpClient.GetAsync($"https://www.biberk.com/policyholders/resources/employer-posting-notices/{state}");
+
+            // Get the response content
+            string responseContent = await response__.Content.ReadAsStringAsync();
+
+            // Check if the server message is present in the response content
+            if (!responseContent.Contains(state))
+            {
+                string serverMessage = $"Expected to see the server message: {state}";
+                Console.WriteLine(serverMessage);
+            }
+
+            // Send an HTTP GET request to the dropdown option URL
+            HttpResponseMessage response___ = await httpClient.GetAsync($"https://www.biberk.com/policyholders/resources/employer-posting-notices/{state}/{state}");
+
+            // Check the response status code
+            if (response___.IsSuccessStatusCode)
+            {
+                Console.WriteLine($"The link for dropdown option {state} is not broken.");
+            }
+            else
+            {
+                string errorMessage = $"The link for dropdown option {state} is broken.";
+                Console.WriteLine(errorMessage);
+            }
+
+            TearDown();
+        }
+
         public void Setup()
         {
             httpClient = new HttpClient();
         }
 
-        [AfterScenario]
         public void TearDown()
         {
             httpClient.Dispose();
         }
+    }
 
-        [Given("I am on the Biberk website")]
-        public void I_am_on_the_Biberk_website()
+    public static class Program
+    {
+        public static async Task Main(string[] args)
         {
-        }
+            var verifier = new LinkVerifier();
 
-        [When("I select the state of (.*)")]
-        public void I_select_the_state_of(string state)
-        {
-            selectedState = state;
-            Thread.Sleep(3000);
-        }
-
-        [When("I click on the link for (.*)")]
-        public void I_click_on_the_link_for(string state)
-        {
-        }
-
-        [Then("I should be taken to the (.*) employer posting notices page")]
-        public async Task I_should_be_taken_to_the_state_employer_posting_notices_page(string state)
-        {
-            HttpResponseMessage response = await httpClient.GetAsync($"https://www.biberk.com/policyholders/resources/employer-posting-notices/{state}");
-
-            pageTitle = response.RequestMessage.RequestUri.AbsoluteUri;
-           Console.WriteLine(pageTitle.Contains(state).ToString(), $"Expected to be taken to the {state} employer posting notices page");
-        }
-
-        [Then("I should see an error message that the page cannot be found")]
-        public async Task I_should_see_an_error_message_that_the_page_cannot_be_found()
-        {
-            HttpResponseMessage response = await httpClient.GetAsync($"https://www.biberk.com/policyholders/resources/employer-posting-notices/");
-
-            // Check the response status code
-            if (!response.IsSuccessStatusCode)
+            List<string> states = new List<string>
             {
-                errorMessage = "Expected to see an error message that the page cannot be found";
-                Console.WriteLine(errorMessage);
-            }
-        }
+                "Delaware",
+                "Illinois",
+                "Indiana",
+                "Kentucky",
+                "New Jersey",
+                "New Mexico",
+                "Vermont",
 
-        [Then("I should see the server message (.*)")]
-        public async Task I_should_see_the_server_message(string message)
-        {
-            // Send an HTTP GET request to the page URL
-            HttpResponseMessage response = await httpClient.GetAsync($"https://www.biberk.com/policyholders/resources/employer-posting-notices/{selectedState}");
+                // Add more states as needed
+            };
 
-            // Get the response content
-            string responseContent = await response.Content.ReadAsStringAsync();
-
-            // Check if the server message is present in the response content
-            if (!responseContent.Contains(message))
+            foreach (string state in states)
             {
-                serverMessage = $"Expected to see the server message: {message}";
-                Console.WriteLine(serverMessage);
-            }
-        }
-
-        [Then("I should check the dropdown link for (.*)")]
-        public async Task I_should_check_the_dropdown_link(string option)
-        {
-            // Send an HTTP GET request to the dropdown option URL
-            HttpResponseMessage response = await httpClient.GetAsync($"https://www.biberk.com/policyholders/resources/employer-posting-notices/{selectedState}/{option}");
-
-            // Check the response status code
-            if (response.IsSuccessStatusCode)
-            {
-                Console.WriteLine($"The link for dropdown option {option} is not broken.");
-            }
-            else
-            {
-                errorMessage = $"The link for dropdown option {option} is broken.";
-                Console.WriteLine(errorMessage);
+                await verifier.VerifyLink(state);
             }
         }
     }
